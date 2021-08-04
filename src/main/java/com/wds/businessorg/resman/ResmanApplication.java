562a5a6cf984693ffc38640eb879b62c2b4095f5
sykq <genericmailacc@gmail.com>
2021-08-04 08:20:48 +0200

    Initial commit

---
+++ src/main/java/com/wds/businessorg/resman/ResmanApplication.java
@@ -0,0 +1,13 @@
+package com.wds.businessorg.resman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResmanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wds/businessorg/resman/ResmanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wds.businessorg.resman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResmanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
