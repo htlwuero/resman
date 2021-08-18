203815c009b1a41c0a88fa4dd9d1862879b51194
LostTripper03 <62254523+LostTripper03@users.noreply.github.com>
2021-08-18 16:04:33 +0200

    update

--- src/main/java/com/wds/businessorg/resman/rest/DepartmentRestController.java
+++ src/main/java/com/wds/businessorg/resman/rest/DepartmentRestController.java
@@ -1,9 +1,11 @@
 package com.wds.businessorg.resman.rest;
 
 import com.wds.businessorg.resman.domain.Department;
+import com.wds.businessorg.resman.domain.Person;
 import com.wds.businessorg.resman.persistence.DepartmentRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -29,5 +31,11 @@ public class DepartmentRestController {
 
         return departments;
     }
+    @GetMapping("departments/{id}")
+    public Department getDepartmentById(@PathVariable int id) {
+        return departmentRepository.findById(id)
+                .orElse(null);
+    }
+
 
 }
